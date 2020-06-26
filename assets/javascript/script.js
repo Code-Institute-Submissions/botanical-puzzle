e231dc25072bbf64fbc7998b4dcd77c8e74697ad
sophie-apples <sophieannemurray@gmail.com>
2020-06-26 07:57:59 +0000

    all map coordinates added to game.html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,30 @@
     $( "#plant-map" ).draggable();
   } );
 
-  // When the map is clicked, the counter will increse by 1
-$(function(){
-  var numberofplants=0;
-$( "#plant-map" ).click(function(){
-    numberofplants++;    
-    $("#numberOfPlants").text(numberofplants);
-});
-})
+  let plants = [
+        {
+            'name': 'Alexanders',
+            'description': 'Found in hedgebanks and waste places near the sea. Edible stems, leaves and flower buds. Flowers April-June.',
+            'image_name': 'alexanders.jpg',
+            'boundary': '776,1926,655,1726',
+        },
+        {
+            'name': 'Apple Mint',
+            'description': 'Apple-scented mint which is tasty in cold drinks.',
+            'image_name': 'whorledmint.jpg',
+            'boundary': '94,98,230,321',
+      },
+      {
+            'name': 'Aurum Lilly',
+            'description': 'Found in shady places. Arrow-shaped glossy leaves, with shiny orange berries in late summer. Poisonous. If the root is baked until dry, it is harmless and can be used like arrowroot.',
+            'image_name': 'aurumlilly.jpg',
+            'boundary': '1071,609,1166,759',
+      },
+      {
+            'name': 'Whorled Mint',
+            'description': 'Found in damp places. A cross between water mint and corn mint. Edible.',
+            'image_name': 'whorledmint.jpg',
+            'boundary': '94,98,230,321',
+      },
+
+  ]
