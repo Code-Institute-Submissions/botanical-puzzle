bd3e8b8d23d2d3627f03faee5d56b612500f127f
sophie-apples <sophieannemurray@gmail.com>
2020-06-26 08:17:49 +0000

    added sample plant array to script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -24,6 +24,24 @@
             'boundary': '1071,609,1166,759',
       },
       {
+            'name': 'Balm',
+            'description': 'Found near hedges in shady places. Yellow-green pointed leaves, white/yellow flowers July-September. Lemon-scented leaves add lemon flaour to food, make a refreshing tea, and can be added to salads.',
+            'image_name': 'balm.jpg',
+            'boundary': '260,1260,440,1438',
+      },
+      {
+            'name': 'Bath Asparagus',
+            'description': 'Found around Bath and Wiltshire. Greyish leaves and pale yellow and green flower spikes from May-July. The unopened flower spikes can be cooked and eaten like asparagus. Belongs to the lily family and is also called Spiked Star-of-Bethlehem.',
+            'image_name': 'bathasparagus.jpg',
+            'boundary': '951,1802,823,1539',
+      },
+      {
+            'name': 'Beech',
+            'description': 'A tree which particularly likes chalky soil. Smooth grey bark and bright green leaves. Nuts from September-October. The nuts can be eaten or used to make beechnut oil and butter; the young leaves can be used to make liquor or eaten in salads; and the wood can be used for fuel and making furniture.',
+            'image_name': 'beech.jpg',
+            'boundary': '2078,243,2253,331',
+      },
+      {
             'name': 'Whorled Mint',
             'description': 'Found in damp places. A cross between water mint and corn mint. Edible.',
             'image_name': 'whorledmint.jpg',
