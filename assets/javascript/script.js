983d6efceb2c243287de5de55ce25a13c77dd5c2
sophie-apples <sophieannemurray@gmail.com>
2020-06-27 09:18:51 +0000

    html checker

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,30 +22,35 @@
             'description': 'Found in shady places. Arrow-shaped glossy leaves, with shiny orange berries in late summer. Poisonous. If the root is baked until dry, it is harmless and can be used like arrowroot.',
             'image_name': 'aurumlilly.jpg',
             'boundary': '1071,609,1166,759',
+            'id': 'aurumlilly'
       },
       {
             'name': 'Balm',
             'description': 'Found near hedges in shady places. Yellow-green pointed leaves, white/yellow flowers July-September. Lemon-scented leaves add lemon flaour to food, make a refreshing tea, and can be added to salads.',
             'image_name': 'balm.png',
             'boundary': '260,1260,440,1438',
+            'id': 'balm'
       },
       {
             'name': 'Bath Asparagus',
             'description': 'Found around Bath and Wiltshire. Greyish leaves and pale yellow and green flower spikes from May-July. The unopened flower spikes can be cooked and eaten like asparagus. Belongs to the lily family and is also called Spiked Star-of-Bethlehem.',
             'image_name': 'bathasparagus.png',
             'boundary': '951,1802,823,1539',
+            'id': 'bathasparagus'
       },
       {
             'name': 'Beech',
             'description': 'A tree found all over the British Isles. Smooth grey bark and bright green leaves. Nuts from September-October. The nuts can be eaten or used to make beechnut oil and butter; the young leaves can be used to make liquor or eaten in salads; and the wood can be used for fuel and making furniture.',
             'image_name': 'beech.png',
             'boundary': '2078,243,2253,331',
+            'id': 'beech'
       },
       {
             'name': 'Whorled Mint',
             'description': 'Found in damp places. A cross between water mint and corn mint. Edible.',
             'image_name': 'whorledmint.png',
             'boundary': '94,98,230,321',
+            'id': 'whorledmint'
       },
 
   ]
@@ -75,13 +80,9 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomPlant() {
-    
-}
-
 // Show an image ('token') to match to image on the picture map
-window.onload = showImage;
 
+window.onload = plantClick;
 function showImage(){
 // Use the random number to select a plant object
     let selectedPlantIndex = (getRandomInt(0, plants.length));
@@ -97,23 +98,24 @@ function showImage(){
     var src = document.getElementById("token");
     src.appendChild(img);
 
+
 }
 
-//Event Handler for the user clicking on the plant image map
 
-window.onload = plantClick;
+
+//Event Handler for the user clicking on any plant on the plant image map
 function plantClick() {
 
     var plantmap = document.getElementById('plantmap');
 
     plantmap.addEventListener('click', anyPlantClick, false);
 
-    function anyPlantClick(e) {
-        if (e.target !== e.currentTarget) {
-            var clickedBtn = e.target.id;
-            alert("Button: " + clickedBtn);
+    function anyPlantClick(selectedPlant) {
+        if (selectedPlant.target !== selectedPlant.currentTarget) {
+            var clickedBtn = selectedPlant.target.title;
+            alert("You found " + clickedBtn + " !");
     }
-        e.stopPropagation();
+        selectedPlant.stopPropagation();
     }
 
 }
