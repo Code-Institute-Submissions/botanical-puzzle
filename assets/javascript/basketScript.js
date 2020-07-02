f371d20e85229a15c49de5b32be9631aec9cbad6
sophie-apples <sophieannemurray@gmail.com>
2020-07-02 20:11:01 +0000

    removing unncessary files

--- assets/javascript/basketScript.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world!");
-
-let titleScore = document.getElementById('title-score');
-let basketScore = score;
-
-function showBasketScore(){
-    
-    titleScore.textContent(basketScore);
-}
